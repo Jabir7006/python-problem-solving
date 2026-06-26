5e8147056003827802d9ce9c5bc219318aa41b01
Jabir7006 <www.roman1249@gmail.com>
2026-06-26 17:54:40 +0600

    feat: implement item counter logic for sales inventory

--- 07_inventory_tracker.py
+++ 07_inventory_tracker.py
@@ -1,10 +1,14 @@
 sales_items = ["Apple", "Banana", "Apple", "Orange", "Apple", "Mango", "Banana"]
 
+# first way
 count = 0
 
 for item in sales_items:
     if item == 'Apple':
         count += 1
-
-
 print(count)
+
+
+# second way (short and easy method)
+count2 = sales_items.count('Apple')
+print(count2)
