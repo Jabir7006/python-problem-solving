a0b025df9edfea77a84c080b96f8d34a98c8df83
Jabir7006 <www.roman1249@gmail.com>
2026-06-26 17:52:30 +0600

    feat: count specific item occurrences in a list using a loop

---
+++ 07_inventory_tracker.py
@@ -0,0 +1,10 @@
+sales_items = ["Apple", "Banana", "Apple", "Orange", "Apple", "Mango", "Banana"]
+
+count = 0
+
+for item in sales_items:
+    if item == 'Apple':
+        count += 1
+
+
+print(count)
