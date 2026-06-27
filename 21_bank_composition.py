6472d45cad3f0aa008d96d48a1517df9c557f84f
Jabir7006 <www.roman1249@gmail.com>
2026-06-27 09:52:01 +0600

    feat: implement Customer class using composition with BankAccount

---
+++ 21_bank_composition.py
@@ -0,0 +1,69 @@
+class BankAccount:
+    total_accounts = 0
+
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self._balance = balance
+        BankAccount.total_accounts += 1
+    @property
+    def balance(self):
+        return self._balance
+
+    @balance.setter
+    def balance(self, value):
+        if value < 0:
+            raise ValueError('Balance cannot be negative!') 
+        self._balance = value
+    
+    def show_details(self):
+        return f'Account Owner: {self.owner}, Current Balance: {self.balance}'
+
+    def deposit(self, n):
+        if n <= 0:
+            raise ValueError('Invalid deposit amount!')
+        self.balance += n
+        print(f'Deposit {n} successfully')
+
+    def withdraw(self, n):
+        if n > self.balance or n <= 0:
+            raise ValueError('Insufficient balance or invalid amount!')
+        self.balance -= n
+        print(f'Withdraw {n} successfully')
+
+    @classmethod
+    def get_total_accounts(cls):
+        return f'Total Bank Accounts: {cls.total_accounts}'
+
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self,owner, balance):
+        super().__init__(owner, balance)
+    
+    def add_interest(self, rate):
+        self.balance += (self.balance * rate) / 100
+
+class CheckingAccount(BankAccount):
+    def __init__(self, owner, balance):
+        super().__init__(owner, balance)
+
+    def withdraw(self, n):
+        super().withdraw(n + 50)
+
+
+
+class Customer:
+    def __init__(self, name, email, account):
+        self.name = name
+        self.email = email 
+        self.account = account
+    
+    def get_customer_info(self):
+        return f'Customer Name: {self.name} \nCustomer Email: {self.email} \nAccount Balance: {self.account.balance}'
+
+
+account1 = BankAccount('Jabir', 1200)
+account2 = BankAccount('Ahmad', 2400)
+
+customer = Customer('Jabir', 'jabirahmad@gmail.com', account1)
+print(customer.get_customer_info())
